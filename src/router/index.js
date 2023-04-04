f86eeccffba2d584929eb9f687a8aadff3a30f22
wpenga <1687219868@qq.com>
2023-04-04 18:15:13 +0800

    设置动态路由，404

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,11 @@ const routes = [
     path: '/register',
     name: 'Register',
     component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('../views/404.vue')
   }
 ]
 
@@ -28,22 +33,41 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-//动态路由
+//刷新会使路由重置
 export const setRoutes= () =>{
-  const storeMenus = localStorage.getItem("menus");
-  //拼装
+  const storeMenus = localStorage.getItem("menus")
+  ////动态路由 拼装
   if(storeMenus){
-    const manageRoute = {path: '/', name: 'Manage', component: () => import('../views/Manage.vue'),redirect:"/home", children:[]}
+    const manageRoute = { path: '/', name: 'Manage', component: () => import('../views/Manage.vue'), redirect: "/home", children: [
+        { path: 'person', name: '个人信息', component: () => import('../views/Person.vue')},
+        { path: 'password', name: '修改密码', component: () => import('../views/Password.vue')}
+        ]}
     const menus = JSON.parse(storeMenus)
     menus.forEach(item =>{
-      let itemMenu = {path: item.path.replace("/",""), name: itme.name, component:() => import(`../view/${item.pagePath}`)}
-      manageRoute.children.push(itemMenu)
+      if (item.path) {  // 当且仅当path不为空的时候才去设置路由
+        let itemMenu = { path: item.path.replace("/", ""), name: item.name, component: () => import('../views/' + item.pagePath + '.vue')}
+        manageRoute.children.push(itemMenu)
+      } else if(item.children.length) {
+        item.children.forEach(item => {
+          if (item.path) {
+            let itemMenu = { path: item.path.replace("/", ""), name: item.name, component: () => import('../views/' + item.pagePath + '.vue')}
+            manageRoute.children.push(itemMenu)
+          }
+        })
+      }
     })
-    //添加到路由对象中
-    router.addRoute(manageRoute)
-  }
+    //获取当前路由对象名称数组
+    const  currentRouteNames = router.getRoutes().map(v => v.name)
+    if(!currentRouteNames.includes('Manage')){
+      //添加到路由对象中
+      router.addRoute(manageRoute)
+      // console.log('添加了'+JSON.stringify(manageRoute))
+    }
 
+  }
 }
+//刷新后重新设置路由
+// setRoutes()
 router.addRoute({
   path: '/',
   name: 'Manage',
@@ -65,6 +89,18 @@ router.addRoute({
 router.beforeEach((to,from, next)=>{//路由名称
   localStorage.setItem("currentPathName", to.name)  //设置路由名称
   index.commit("setPath")  ///触发store的数据更新
+
+  // 未找到路由的情况
+  if (!to.matched.length) {
+    const storeMenus = localStorage.getItem("menus")
+    if (storeMenus) {
+      next("/404")
+    } else {
+      // 跳回登录页面
+      next("/login")
+    }
+  }
+
   next()//放行路由
 })
 export default router
